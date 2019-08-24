e90d795499422866ea7b1bf79bbd88a65630befb
show50726 <k40526@hotmail.com>
2019-08-24 17:09:45 +0800

    death trigger and reset to spawn point

--- Assets/Scripts/WorldManager.cs
+++ Assets/Scripts/WorldManager.cs
@@ -7,17 +7,27 @@ public class WorldManager : MonoBehaviour
     public GameObject NormalWorld;
     public GameObject InsideWorld;
     public float changeDuration = 3f;
+    public Player player;
+    public Vector3 spawnPoint;
     // Start is called before the first frame update
     void Start()
     {
+
         NormalWorld.SetActive(true);
         InsideWorld.SetActive(false);
+        spawnPoint = player.gameObject.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(player.isDead){
+            Debug.Log("Dead");
+            player.gameObject.SetActive(false);
+            player.transform.position = spawnPoint;
+            player.gameObject.SetActive(true);
+            player.isDead = false;
+        }   
     }
 
     public void ChangeWorld(){
