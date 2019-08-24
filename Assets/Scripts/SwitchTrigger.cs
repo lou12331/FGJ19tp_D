c4fec6dc97ade9c603c1dcdd8cdd20ad1c34bc9e
show50726 <k40526@hotmail.com>
2019-08-24 15:36:19 +0800

    update trigger

--- Assets/Scripts/SwitchTrigger.cs
+++ Assets/Scripts/SwitchTrigger.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Test : MonoBehaviour
+public class SwitchTrigger : MonoBehaviour
 {
     public WorldManager worldManager;
     private BoxCollider2D box;
