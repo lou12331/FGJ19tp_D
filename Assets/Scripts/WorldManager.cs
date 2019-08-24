efe9dd170593679db7625f37678ae3f15fe93815
show50726 <k40526@hotmail.com>
2019-08-24 15:30:04 +0800

    world changing script

---
+++ Assets/Scripts/WorldManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldManager : MonoBehaviour
+{
+    public GameObject NormalWorld;
+    public GameObject InsideWorld;
+    public float changeDuration = 3f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        NormalWorld.SetActive(true);
+        InsideWorld.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ChangeWorld(){
+        // Animation
+        InsideWorld.SetActive(true);
+        NormalWorld.SetActive(false);
+        StartCoroutine(counter(changeDuration));
+        
+
+    }
+
+    IEnumerator counter(float d){
+        yield return new WaitForSeconds(d);
+        InsideWorld.SetActive(false);
+        NormalWorld.SetActive(true);
+    }
+
+
+}
