74e472520d6c00a79ff0248781dd6553e148c366
Steven Chao <gash612@gmail.com>
2019-08-25 02:20:17 +0800

    [Tutorial Stage] Some Stages For tutorial

---
+++ Assets/Scripts/MenuAction.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuAction : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void onclickStart()
+    {
+        Application.LoadLevel("TestScene1");
+    }
+
+    public void onclickBWStart()
+    {
+        Application.LoadLevel("Tutorial1");
+    }
+
+
+}
