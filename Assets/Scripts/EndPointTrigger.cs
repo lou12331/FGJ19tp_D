8fcae7b057f27fd5ab6a77c9f6df422692921e86
Louis <lou12331@gmail.com>
2019-08-25 14:00:44 +0800

    Assets\Scripts\EndPointTrigger.cs Assets\Scripts\Player.cs Assets\Scripts\WorldManager.cs

--- Assets/Scripts/EndPointTrigger.cs
+++ Assets/Scripts/EndPointTrigger.cs
@@ -29,7 +29,8 @@ public class EndPointTrigger : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D other) {
         if(other.gameObject.tag == "Player"){
-            other.gameObject.GetComponent<Movement>().canMove = false;
+
+            WorldManager.instance.PlayerState(false);
             endStage = true;
             audio.enabled = true;
             audio.Play();
